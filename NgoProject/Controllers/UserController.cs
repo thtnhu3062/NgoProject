e2fbf9205ae83d8ad47ca0a8967203133764e6ad
melanie200422 <116139837+melanie200422@users.noreply.github.com>
2023-12-15 08:41:02 +0700

    ss
    sss

---
+++ NgoProject/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NgoProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Donate()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
