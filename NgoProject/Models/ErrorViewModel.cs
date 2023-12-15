3a4e510012af197673ce85b437819b8a6980f4ce
melanie200422 <116139837+melanie200422@users.noreply.github.com>
2023-12-15 08:37:38 +0700

    sxc
    a

--- NgoProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NgoProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
