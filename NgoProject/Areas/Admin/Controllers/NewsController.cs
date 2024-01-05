aec40250ea675f88c1a2547894bd03e1b9505284
Elnino090292 <114639587+Elnino090292@users.noreply.github.com>
2024-01-05 08:56:22 +0700

    up
    up date

---
+++ NgoProject/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using NgoProject.Models;
+using NgoProject.ViewModel;
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace NgoProject.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/News")]
+    public class NewsController : Controller
+    {
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly NgoProjectContext db;
+
+        public NewsController(NgoProjectContext db, IWebHostEnvironment hostEnvironment)
+        {
+            this.db = db;
+            this._hostEnvironment = hostEnvironment;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [Route("News")]
+        [HttpGet]
+        public async Task<IActionResult> News()
+        {   
+            ViewBag.CategoryId = new SelectList(db.Categories.ToList(), "CategoryId", "CategoryName");
+            ViewBag.OurpartnerId = new SelectList(db.Ourpartners.ToList(), "OurpartnerId", "OurpartnerName");
+
+            return View(await db.News.OrderByDescending(p=>p.NewsId).Include(p=>p.Category).Include(p=>p.Ourpartner).ToListAsync());
+
+
+        }
+
+        [Route("NewsAdd")]
+        [HttpGet]
+        public IActionResult NewsAdd()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories.ToList(), "CategoryId", "CategoryName");
+            ViewBag.OurpartnerId = new SelectList(db.Ourpartners.ToList(), "OurpartnerId", "OurpartnerName");
+            return View();
+        }
+        [Route("NewsAdd")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> NewsAdd(ViewModelNews model)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                string imageFilename1 = string.Empty;
+                if (model.NewsImage1 != null && model.NewsImage1.Length > 0)
+                {
+                    imageFilename1 = model.NewsImage1.FileName;
+                    var imgFolder = Path.Combine(_hostEnvironment.WebRootPath, "user/images/slide");
+                    if (!Directory.Exists(imgFolder))
+                    {
+                        Directory.CreateDirectory(imgFolder);
+
+                    }
+                    var imgPath = Path.Combine(imgFolder, imageFilename1);
+                    var fs = new FileStream(imgPath, FileMode.OpenOrCreate);
+                    await model.NewsImage1.CopyToAsync(fs);
+                }
+
+                News p = new News
+                {
+
+                    NewsName = model.NewsName,
+                    NewsContent = model.NewsContent,
+                    NewsDescription = model.NewsDescription,
+                    NewsImage1 = imageFilename1,
+
+                };
+
+
+                db.News!.Add(p);
+                await db.SaveChangesAsync();
+                return RedirectToAction("News", "News");
+            }
+            return View(model);
+
+        }
+    }
+
+
+}
---
+++ NgoProject/ViewModel/ViewModelNews.cs
@@ -0,0 +1,32 @@
+﻿using NgoProject.Models;
+using System.ComponentModel.DataAnnotations;
+namespace NgoProject.ViewModel
+{
+    public class ViewModelNews
+    {
+        [Key]
+        public int NewsId { get; set; }
+
+        public string? NewsName { get; set; }
+
+        public IFormFile? NewsImage1 { get; set; }
+
+        public IFormFile? NewsImage2 { get; set; }
+
+        public IFormFile? NewsImage3 { get; set; }
+
+        public string? NewsContent { get; set; }
+
+        public string? NewsDescription { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public int? OurpartnerId { get; set; }
+
+        public virtual Category? Category { get; set; }
+
+        public virtual ICollection<Donate> Donates { get; set; } = new List<Donate>();
+
+        public virtual Ourpartner? Ourpartner { get; set; }
+    }
+}
